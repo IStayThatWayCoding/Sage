79fb6b95af50a2769ad4f1da8f249651ad7f7446
istay <istaythatway@gmail.com>
2024-03-26 04:32:28 -0400

    Update ping.js

--- src/commands/slash/utilities/ping.js
+++ src/commands/slash/utilities/ping.js
@@ -18,25 +18,25 @@ module.exports = {
    * @param {ChatInputCommandInteraction} interaction
    */
   run: async (bot, interaction) => {
-    // const message = await interaction.deferReply({
-    //     fetchReply: true
-    // });
-
-    // const newMessage = `API Latency: ${bot.ws.ping}\nBot Ping: ${message.createdTimestamp - interaction.createdTimestamp}`
-    // await interaction.editReply({
-    //     content: newMessage
-    // })
-
-    await interaction.reply({
-      content: `Select one of the components below. ${process.env.CURRENCY}`,
-      components: [
-        new ActionRowBuilder().addComponents(
-          new ButtonBuilder()
-            .setCustomId("example-button")
-            .setLabel("Example Button")
-            .setStyle(1)
-        ),
-      ],
+    const message = await interaction.deferReply({
+        fetchReply: true
     });
+
+    const newMessage = `API Latency: ${bot.ws.ping}\nBot Ping: ${message.createdTimestamp - interaction.createdTimestamp}`
+    await interaction.editReply({
+        content: newMessage
+    })
+
+    // await interaction.reply({
+    //   content: `Select one of the components below. ${process.env.CURRENCY}`,
+    //   components: [
+    //     new ActionRowBuilder().addComponents(
+    //       new ButtonBuilder()
+    //         .setCustomId("example-button")
+    //         .setLabel("Example Button")
+    //         .setStyle(1)
+    //     ),
+    //   ],
+    // });
   },
 };
