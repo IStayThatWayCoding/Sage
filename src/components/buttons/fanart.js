5fb4466c9aba0fd9152841c15de880e094d6b133
istay <68560774+IStayThatWayCoding@users.noreply.github.com>
2024-10-18 22:58:49 -0400

    Update fanart.js

--- src/components/buttons/fanart.js
+++ src/components/buttons/fanart.js
@@ -27,7 +27,7 @@ module.exports = {
       //     "If you have any fan art that is **original** and that can be used in server-wide designs, such as logos and banners, post here! Be creative! If you need Oasis's (Swiftblade's) character sheet you can access it below. If you have any questions, please contact staff."
       //   )
       .setDescription(
-        "If you have any fanart that is original post it here! Be creative! Oasis's (SwiftBlade's) character sheet is attached below if needed. If you have any questions regarding fanart boundaries, visit the link below.\n\n:link: **[Click Here](https://oasisyt.carrd.co/#begin)**"
+        "If you have any fanart that is original post it here! Be creative! Oasis's (Swiftblade's) character sheet is attached below if needed. If you have any questions regarding fanart boundaries, visit the link below.\n\n:link: **[Click Here](https://oasisyt.carrd.co/#begin)**"
       )
       .setImage(`attachment://character-sheet.jpg`);
 
